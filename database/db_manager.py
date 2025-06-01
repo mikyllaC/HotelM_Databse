291bb18141640fee9c8ea78986bc4dd2d280b048
Samuel Muralid <muralidharansamuel@gmail.com>
2025-06-01 23:54:11 +0800

    added database logic for creating employee table

--- database/db_manager.py
+++ database/db_manager.py
@@ -5,12 +5,31 @@ import os
 def main():
     db = DBManager()
 
+
 # ============== Database Manager Class ==============
 class DBManager:
     def __init__(self):
-        # Dynamically set the database path relative to this file
         base_dir = os.path.dirname(os.path.abspath(__file__))
         self.db_path = os.path.join(base_dir, "Hotel_Management.db")
+        # os.path.abspath(__file__) gives the full path to db_manager.py.
+        # os.path.dirname(...) gives the directory (.../database/).
+        # os.path.join(...) builds the final path to Hotel_Management.db.
+
+        self.create_employee_table()
+
+
+    def create_employee_table(self):
+        with sqlite3.connect(self.db_path) as conn:
+            cursor = conn.cursor()
+
+            cursor.execute("""
+            CREATE TABLE IF NOT EXISTS EMPLOYEE
+                employee_id TEXT PRIMARY KEY
+                first_name TEXT NOT NULL
+                last_name TEXT NOT NULL
+                position TEXT NOT NULL""")
+            conn.commit()
+
 
 
 if __name__ == "__main__":
