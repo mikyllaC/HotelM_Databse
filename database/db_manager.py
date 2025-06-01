f1c328b790028ff3ccb955e322eda61e6f176ee7
Samuel Muralid <muralidharansamuel@gmail.com>
2025-06-01 23:55:09 +0800

    added database logic for generating employee id (initials+incrementing id)

--- database/db_manager.py
+++ database/db_manager.py
@@ -31,6 +31,26 @@ class DBManager:
             conn.commit()
 
 
+    def generate_employee_id(self, first_name, last_name):
+        initials = (first_name[0] + last_name[0]).upper()
+
+        with sqlite3.connect(self.db_path) as conn:
+            cursor = conn.cursor()
+
+            cursor.execute("""
+            SELECT employee_id 
+            FROM EMPLOYEE
+            ORDER BY CAST(SUBSTR(employee_id, 3) AS INTEGER) DESC
+            LIMIT 1
+            """)
+            # SUBSTR(employee_id, 3) — skips the first two characters (the initials).
+            # CAST(... AS INTEGER) — converts the numeric suffix to an integer.
+            # Orders descending by this integer to find the max. LIMIT gets the max
+            result = cursor.fetchone()
+            print(result)
+            conn.commit()
+
+
 
 if __name__ == "__main__":
     main()
